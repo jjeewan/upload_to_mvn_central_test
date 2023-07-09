1957d1cf46330ef6024fb9765f27a190b28fd060
Jeewan Jyoti <jeewan.jyoti@oracle.com>
2023-07-09 08:03:01 +0530

    removed oracle codes

---
+++ src/main/java/io/github/jjeewan/Main.java
@@ -0,0 +1,7 @@
+package io.github.jitendrakumar2006;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Spring Cloud OCI Upload to Maven Central Testing");
+    }
+}
